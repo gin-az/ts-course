fcf02823ef1a33f56a6b61c954d25c5435ca8640
AGindullin <GA>
2022-03-10 20:43:39 +0300

    #Получение данных axios jsonplaceholder.typicode

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Card, {CardVariant} from "./components/Card";
 import UserList from "./components/UserList";
 import {IUser} from "./types/type";
+import axios from "axios";
 
 // HARDCORE MOCK
-const users: IUser[] = [
-  {id: 1, name: 'Oleg', email: 'assembler@ml.ru', address:{city: 'Москва', street:"Тверская-Ямская", zipcode: '123'}},
-  {id: 2, name: 'TIMUR', email: 'delphi@ml.ru', address:{city: 'Уфа', street:"Ленина", zipcode: '450000'}},
-
-]
+// const users: IUser[] = [
+//   {id: 1, name: 'Oleg', email: 'assembler@ml.ru', address:{city: 'Москва', street:"Тверская-Ямская", zipcode: '123'}},
+//   {id: 2, name: 'TIMUR', email: 'delphi@ml.ru', address:{city: 'Уфа', street:"Ленина", zipcode: '450000'}},
+// ]
 
 const App = () => {
+  const [users, setUsers] = useState<IUser[]>([])
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+      setUsers(response.data);
+    } catch (e) {
+      alert(e)
+    }
+  }
+
   return (
     <div>
        <Card onClick={(num) => console.log('click', num)} height='250px' width='250px' variant={CardVariant.primary}>
