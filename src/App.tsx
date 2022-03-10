1e267992ca0c5db5c23c97ac1e75c6cb68fd43ee
AGindullin <GA>
2022-03-10 15:42:51 +0300

    #2 props 1) Компонент Card явл-ся функциональным компонентом с обобщенным типом(generic): FC<CardProps> 2) передача функции в пропсах.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Card from "./components/Card";
+import Card, {CardVariant} from "./components/Card";
 
 const App = () => {
   return (
     <div>
-       <Card height='250px' width='250px'>
+       <Card onClick={(num) => console.log('click', num)} height='250px' width='250px' variant={CardVariant.primary}>
          <button>Кнопка</button>
          <p>Абзац</p>
        </Card>
