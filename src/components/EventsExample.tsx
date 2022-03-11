d3e723f7ab183c6cb1503a071b9681c797981f49
AGindullin <GA>
2022-03-11 15:28:35 +0300

    #Типизация событий 1) Неуправляемый input. Типизация хука useRef.

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -1,15 +1,18 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useRef, useState} from 'react';
 import {log} from "util";
 
 const EventsExample: FC = () => {
   const [value, setValue] = useState<string>('');
   const [isDrag, setIsDrag] = useState<boolean>(false);
+  // неуправляемым input работа ведется через useRef
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
   const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log(value);
+    console.log('value: ', value);
+    console.log('inputRef: ', inputRef.current?.value);
   };
 
   const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
@@ -33,7 +36,8 @@ const EventsExample: FC = () => {
 
   return (
     <div>
-      <input value={value} onChange={changeHandler} type="text" />
+      <input value={value} onChange={changeHandler} type="text" placeholder="Управляемый" />
+      <input ref={inputRef} type="text" placeholder="Неуправляемый" />
       <button onClick={clickHandler}>Вывести значение в консоль</button>
       <div onDrag={dragHandler} draggable style={{width: 200, height: 200, background: '#dd5566'}}></div>
       <div
@@ -43,6 +47,7 @@ const EventsExample: FC = () => {
         style={{width: 200, height: 200, background: isDrag ? '#3355ee' : '#dd5566', marginTop: 10}}>
           Перетащите 1-ый квадрат на 2-ой.
       </div>
+
     </div>
   );
 };
